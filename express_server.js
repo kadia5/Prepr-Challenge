4c03c991859828c02cf716d96afb560ccc629ef6
Kadia5 <kdabraeux@gmail.com>
2022-04-12 14:32:00 -0400

    install ejs default engine, new page setup, new route setup to urls_new

--- express_server.js
+++ express_server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const nodemon = require('nodemon');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8000; //default port 8000
+const ENV = process.env.ENV || 'development';
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set ('view engine', 'ejs');
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,6 +24,23 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  res.send("urls page!");
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
+app.post("/urls", (req,res)=> {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+})
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
